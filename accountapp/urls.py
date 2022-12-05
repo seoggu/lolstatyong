e00d9ca9fc3c05c2c4fcd6db757fe28964153392
seoggu <pshn123@naver.com>
2022-12-05 21:48:31 +0900

    magicgrid

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
 
--- lolstatyong/urls.py
+++ lolstatyong/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('',include('rankstatapp.urls')),
     path('accounts/',include('accountapp.urls')),
     path('profiles/',include('profileapp.urls')),
-]
+    path('articles/',include('articleapp.urls')),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
