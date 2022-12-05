131afbcbfc4baceba4d77612280293e915cf635c
seoggu <pshn123@naver.com>
2022-12-05 16:49:52 +0900

    profile create

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from profileapp.views import ProfileCreateView
+
 app_name = 'profileapp'
 
 urlpatterns = [
-
+    path('create/', ProfileCreateView.as_view(), name='create'),
 ]
