d998c421e9ac538ea38b5b69817462f7b106a9a4
seoggu <pshn123@naver.com>
2022-12-07 17:28:11 +0900

    project connect mixin

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,10 +6,12 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
+from django.views.generic.list import MultipleObjectMixin
 from accountapp.decorators import account_ownership_required
 
 from accountapp.models import HelloWorld2
 from accountapp.templates.accountapp.forms import AccountUpdateForm
+from articleapp.models import Article
 
 # Create your views here.
 
@@ -40,10 +42,16 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
     
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView,MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+    
+    paginate_by = 10
+    
+    def get_context_data(self, **kwargs):
+        object_list= Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 @method_decorator(has_ownership,'get')
 @method_decorator(has_ownership,'post')
